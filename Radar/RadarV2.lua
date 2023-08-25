0798846b07c7835d6a898f42a82fdf12b23534bb
Ozzy-P <97415717+Ozzy-P@users.noreply.github.com>
2023-08-24 21:19:49 -0700

    Update RadarV2.lua

--- Radar/RadarV2.lua
+++ Radar/RadarV2.lua
@@ -1,6 +1,6 @@
 -- Placeholder file, mostly an idea board at the moment.
 --[[ TODO: 
-* Delegate a scheduler based on current frame rotation -> Get objects currently at that angle from the camera (P: Ignore rescans of same object when camera is reangled.)
+* [MARKED AS DONE] Delegate a scheduler based on current frame rotation -> Get objects currently at that angle from the camera (P: Ignore rescans of same object when camera is reangled.)
 * Move blips on radar relative to camera movement.
 * Fix scaling [among other stuff from V1]
 --]]
